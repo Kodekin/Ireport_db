14725707ae80e6cb0f737bdf29821a5eba509971
tomkeith <tomah5714@gmail.com>
2018-12-15 10:09:31 +0300

    [#finishes 162447374] write tests and add coverage badge on readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 [![Build Status](https://travis-ci.com/tomkeith/Ireport_db.svg?branch=develop)](https://travis-ci.com/tomkeith/Ireport_db)
 [![Maintainability](https://api.codeclimate.com/v1/badges/ca2bfb8b0710f13036a5/maintainability)](https://codeclimate.com/github/tomkeith/Ireport_db/maintainability)
+[![codecov](https://codecov.io/gh/tomkeith/Ireport_db/branch/develop/graph/badge.svg)](https://codecov.io/gh/tomkeith/Ireport_db)
+
--- app/tests/v2/test_incidents.py
+++ app/tests/v2/test_incidents.py
@@ -39,6 +39,7 @@ class AuthTest(unittest.TestCase):
                                   content_type='application/json')
         return result
 
+
     def create_incident(self, path='/v2/redflags', data={}):
         user = self.create_user()
         token = user.json['Auth-token']
@@ -56,6 +57,22 @@ class AuthTest(unittest.TestCase):
         result = self.client.get(path, headers=headers, content_type='application/json')        
         return result
 
+    def get_delete_flags(self, path='/v2/redflags/1'):
+        user = self.create_user()
+        token = user.json['Auth-token']
+        headers = {"Authorization": "Bearer {}".format(token)}
+        result = self.client.delete(path, headers=headers, content_type='application/json')        
+        return result
+
+    def edit_flags(self, path='/v2/redflags/1'):
+        """Test that a user can edit incident that they have posted"""
+        user = self.create_user()
+        token = user.json['Auth-token']
+        headers = {"Authorization": "Bearer {}".format(token)}
+        data = {"location": "edited location"}
+        result = self.client.put(path, data=json.dumps(data), headers=headers, content_type='application/json')
+        return result
+
     def test_post_incident(self):
         new_flag = self.create_incident()
         self.assertEqual(new_flag.status_code, 201)
@@ -63,6 +80,19 @@ class AuthTest(unittest.TestCase):
     def test_get_incidents(self):
         incidents = self.get_flags()
         self.assertEqual(incidents.status_code, 200)
+
+    def test_edit_incident(self):
+        """Test that users can edit an incident that they have posted"""
+        incident = self.edit_flags()
+        self.assertEqual(incident.status_code, 200)
+        self.assertEqual(incident.json["Message"], "location updated successfully")
+
+
+    def test_delete_incident(self):
+        """Test that users can delete an incident that they have posted"""
+        incident = self.get_delete_flags()
+        self.assertEqual(incident.status_code, 200)
+        self.assertEqual(incident.json["Message"], "Id 1 deleted successfully")
 
 
     def tearDown(self):
